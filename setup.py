c27a893123e2fe513df4173a00e316db1ae283fe
VladP1skovy1 <piskovijvlad1@gmail.com>
2025-12-17 17:08:39 +0100

    ros2 setup and cleaning

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup, find_packages
 
-d = generate_distutils_setup(
-    packages=['monodepth_navigation'],
-    package_dir={'': 'src'}
+package_name = 'monodepth_navigation'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tom',
+    maintainer_email='tom@todo.todo',
+    description='ROS2 port of monodepth',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'test_script = monodepth.test_script:main',
+        ],
+    },
 )
-
-setup(**d)
